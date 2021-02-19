73ab1f85f7117457b1a2d80f6c5156ff1e8a650d
benjaminsainz <77997099+benjaminsainz@users.noreply.github.com>
2021-02-19 14:21:28 -0600

    Update ind.py

--- ind.py
+++ ind.py
@@ -5,14 +5,15 @@ Héctor Gibrán Ceballos-Cancino, Francisco Javier Cantu-Ortiz
 
 from sklearn.cluster import KMeans
 import numpy as np
-def km_gen(n_clusters, X):
-    kmeans = KMeans(n_clusters, max_iter=np.random.randint(1,100)) # alternative: init='random'
-    KMmodel = kmeans.fit(X)
-    return list(KMmodel.labels_)
 
 def random_gen(n_clusters, X):
     k_set = []
     for i in range(n_clusters): k_set.append(i)
-    ind=[]
-    for i in range(len(X)): ind.append(k_set[np.random.randint(0,len(k_set))])
+    flag = False
+    while flag is False:
+        ind=[]
+        for i in range(len(X)): ind.append(k_set[np.random.randint(0,len(k_set))])
+        flag = True
+        for k in k_set: 
+            if k not in ind: flag=False
     return ind
