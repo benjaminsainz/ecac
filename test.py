8e00f1b6222e74fdad1c62860fb3a4f054cf9dc5
benjaminsainz <77997099+benjaminsainz@users.noreply.github.com>
2021-05-06 11:48:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -13,10 +13,6 @@ def test(ds, pop_size=20, max_gens=2000, runs=10):
         ecac_run(X, n_clusters, data, pop_size, max_gens, runs=runs, y=y, log_file=False, evolutionary_plot=False)
 
 
-full = ['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 'liver', 'transfusion', 'wine',
-        'knowledge', 'segment', 'congress', 'zoo', 'heart', 'ionosphere', 'r15', 'spiral', 'breast-cancer-wisconsin',
-        'jain', 'titanic', 'parkinsons', 'seeds', 'urban-land-cover', 'pathbased', 'aggregation', 'wholesale']
+paper = ['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 'liver', 'transfusion', 'wine']
 
-test(['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 'liver', 'transfusion', 'wine',
-      'segment', 'zoo', 'r15', 'spiral', 'breast-cancer-wisconsin', 'jain', 'parkinsons', 'seeds', 'pathbased',
-      'aggregation', ])
+test(paper)
