5a15f465a1c93f4abda01885de0f220c5f945082
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-06 17:09:48 -0500

    Update retr.py

--- retr.py
+++ retr.py
@@ -10,52 +10,50 @@ import pandas as pd
 
 
 def retrieval(data):
-    if data == 'iris':
-        n_clusters = 3
-        X, y = datasets.load_iris().data, datasets.load_iris().target
-    elif data == 'wine':
-        n_clusters = 3
-        X, y = datasets.load_wine().data, datasets.load_wine().target
+    if data == 'breast-tissue':
+        n_clusters = 6
+        X = np.array(list(csv.reader(open('data/breast-tissue_X.csv', newline='', encoding='utf-8-sig'))))
+        y = list(csv.reader(open('data/breast-tissue_y.csv', newline='', encoding='utf-8-sig')))
+        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
+    elif data == 'dermatology':
+        n_clusters = 6
+        X = np.array(list(csv.reader(open('data/dermatology_X.csv', newline='', encoding='utf-8-sig'))))
+        y = list(csv.reader(open('data/dermatology_y.csv', newline='', encoding='utf-8-sig')))
+        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'ecoli':
         n_clusters = 8
         X = np.array(list(csv.reader(open('data/ecoli_X.csv', newline=''))))
         y = list(csv.reader(open('data/ecoli_y.csv', newline='')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'glass':
-        n_clusters = 6
-        X = np.array(list(csv.reader(open('data/glass_X.csv', newline=''))))
-        y = list(csv.reader(open('data/glass_y.csv', newline='')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'transfusion':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/transfusion_X.csv', newline=''))))
-        y = list(csv.reader(open('data/transfusion_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'forest':
         n_clusters = 4
         X = np.array(list(csv.reader(open('data/forest_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/forest_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'breast-tissue':
+    elif data == 'glass':
         n_clusters = 6
-        X = np.array(list(csv.reader(open('data/breast-tissue_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/breast-tissue_y.csv', newline='', encoding='utf-8-sig')))
+        X = np.array(list(csv.reader(open('data/glass_X.csv', newline=''))))
+        y = list(csv.reader(open('data/glass_y.csv', newline='')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
+    elif data == 'iris':
+        n_clusters = 3
+        X, y = datasets.load_iris().data, datasets.load_iris().target
     elif data == 'leaf':
         n_clusters = 36
         X = np.array(list(csv.reader(open('data/leaf_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/leaf_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'dermatology':
-        n_clusters = 6
-        X = pd.read_csv('data/dermatology_X.csv', header=None)
-        X.replace({'?': '3'}, inplace=True)
-        X = np.array(X)
-        y = list(csv.reader(open('data/dermatology_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'liver':
         n_clusters = 16
         X = np.array(list(csv.reader(open('data/liver_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/liver_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
+    elif data == 'transfusion':
+        n_clusters = 2
+        X = np.array(list(csv.reader(open('data/transfusion_X.csv', newline=''))))
+        y = list(csv.reader(open('data/transfusion_y.csv', newline='', encoding='utf-8-sig')))
+        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
+    elif data == 'wine':
+        n_clusters = 3
+        X, y = datasets.load_wine().data, datasets.load_wine().target
     return data, n_clusters, X, y
