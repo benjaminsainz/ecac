0f30c39b7a117f6485cb6825a4fa709b73c8f93a
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-06 17:11:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ from retr import *
 from gen import *
 
 
-def test(ds, pop_size=20, max_gens=2000, runs=10):
+full = ['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 
+        'liver', 'transfusion', 'wine']
+
+
+def test(ds=full, pop_size=20, max_gens=2000, runs=10):
     for d in ds:
         data, n_clusters, X, y = retrieval(d)
         ecac_run(X, n_clusters, data, pop_size, max_gens, runs=runs, y=y, log_file=False, evolutionary_plot=False)
 
 
-paper = ['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 'liver', 'transfusion', 'wine']
-
-test(paper)
+test()
